e8c797961550ada1550e1670f7ab475b5d345707
Nakamori Hiroki <hiroki.nakamori@himacs.co.jp>
2025-06-12 17:49:09 +0900

    EditReceiptクラスを更新したよ

--- src/nakamori/EditReceipt.java
+++ src/nakamori/EditReceipt.java
@@ -1,5 +1,18 @@
 package nakamori;
 
+import System.ID_Pass;
+
 public class EditReceipt {
+	
+	int menuEdit ;
+
+
+	
+	//-------------------------------------------
+	// 注文を追加するメソッド
+	public static void insert ( ) {
+		String url = ID_Pass.url ;
+		String user_name = ID_Pass.user_name ;
+		String password = ID_Pass.password ;
 
 }

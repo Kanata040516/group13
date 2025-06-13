a7706e8a7cfcef4a5012e6122d1c63230d764fff
斜森真実 <@EDH25205.mshome.net>
2025-06-13 12:12:27 +0900

    Newの削除とコードの修正

--- src/nanamori/Menu_employee.java
+++ src/nanamori/Menu_employee.java
@@ -6,17 +6,6 @@ public class Menu_employee {
 
 	private static int mainmenu  ;//メインメニューの番号を格納する変数
 	
-//	Main main = new Main();
-//	
-//	Receipt receipt = new Receipt();
-//	
-//	Customer customer = new Customer();
-//	
-//	Item item = new Item();
-//	
-//	Sales sales = new Sales();
-	
-	
 	public static int menu_select() {
 		
 		System.out.println("実行する操作を選んでください。\n0.ホーム画面へ戻る\n1.注文管理\n2.顧客管理\n3.商品管理\n4.売上管理\n5.データ分析");
@@ -28,27 +17,28 @@ public class Menu_employee {
 		
 		if(mainmenu == 0) {//ホームに戻る
 			System.out.println("初期画面に移動します。");
-			//main.home();
+			//Main.home();
+			
 		}
 		
 		else if(mainmenu == 1) {//注文管理画面に移動
 			System.out.println("注文管理画面に移動します。");
-			//receipt.receipt();
+			//Receipt.receipt();
 		}
 		
 		else if(mainmenu == 2) {//顧客管理画面に移動
 			System.out.println("顧客管理画面に移動します。");
-			//customer.customer();
+			//Customer.customer();
 		}
 		
 		else if(mainmenu == 3) {//商品管理画面に移動
 			System.out.println("商品管理画面に移動します。");
-			//item.item();
+			//Item.item();
 		}
 		
 		else if(mainmenu == 4) {//売上管理画面に移動
 			System.out.println("売上管理画面に移動します。");
-			//sales.sales();
+			//Sales.sales();
 		}
 			
 		return mainmenu;//mainmenuの値を返す

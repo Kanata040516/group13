e7d4beb490f040ad591382f93ef02977d74fd32d
斜森真実 <@EDH25205.mshome.net>
2025-06-13 14:50:03 +0900

    Menu_employee:データ分析の削除

--- src/nanamori/Menu_employee.java
+++ src/nanamori/Menu_employee.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Menu_employee {
 
-	private static int mainmenu  ;//メインメニューの番号を格納する変数
+	protected static int mainmenu  ;//メインメニューの番号を格納する変数
 	
 	public static void menu_select() {
 		
-		System.out.println("実行する操作を選んでください。\n0.ホーム画面へ戻る\n1.注文管理\n2.顧客管理\n3.商品管理\n4.売上管理\n5.データ分析");
+		System.out.println("実行する操作を選んでください。\n0.ホーム画面へ戻る\n1.注文管理\n2.顧客管理\n3.商品管理\n4.売上管理");
 		Scanner check = new Scanner(System.in);
 		
 		
--- src/nanamori/Menu_master.java
+++ src/nanamori/Menu_master.java
@@ -1,5 +1,58 @@
 package nanamori;
 
-public class Menu_master {
+import java.util.Scanner;
 
+public class Menu_master extends Menu_employee{
+
+
+	public static void menu_master() {//管理者画面での処理。
+		
+		Scanner check = new Scanner(System.in);
+
+		mainmenu = check.nextInt();//入力した値をmainmenuに入れる
+		
+		if(mainmenu == 0) {//ホーム画面に移動する。
+			System.out.println("初期画面に移動します。");
+			//Main.home;
+		}
+		
+		if(mainmenu == 1) {//注文編集画面に移動する。
+			System.out.println("注文編集画面に移動します。");
+			//Editreceipt.???;//???の部分はどのメソッドを実行するか不明
+		}
+		
+		if(mainmenu == 2) {//商品編集画面に移動する。
+			System.out.println("商品編集画面に移動します。");
+			//EditItem.???;//???の部分はどのメソッドを実行するか不明
+		}
+		
+		if(mainmenu == 3) {//顧客編集画面に移動する。
+			System.out.println("顧客編集画面に移動します。");
+			//EditCustomer.???;//???の部分はどのメソッドを実行するか不明
+		}
+		
+		if(mainmenu == 4) {//従業員編集画面に移動する。
+			System.out.println("従業員編集画面に移動します。");
+			//EditMember.???;//???の部分はどのメソッドを実行するか不明
+		}
+		
+		if(mainmenu == 5) {//管理者閲覧画面に移動する。
+			System.out.println("管理者閲覧画面に移動します。");
+			menu_select();
+		}
+		
+		
+	}
+	
+	public static void menu_select() {//マスター視点のmenu_select処理
+		
+		if(mainmenu == 6) {//管理者編集画面に移行する。
+			System.out.println("管理者編集画面に移動します。");
+			menu_master();
+		}
+	}
+	
+	
 }
+
+

19d90cc60e8ccfd52741d7f9e1885ee3c69dae8d
B <xxx@xxx.jp>
2025-06-17 15:20:59 +0900

    Sales

--- src/miyakoshi/Sales.java
+++ src/miyakoshi/Sales.java
@@ -1,5 +1,4 @@
 package miyakoshi;
-
 import java.util.Scanner;
 
 import nanamori.Menu_employee;
@@ -9,16 +8,17 @@ import yoshida.Judge_pass_id;
 import yoshida.Main;
 
 public class Sales {
-
+ 
 	static int menu = 0;//0-6, 注文検索の項目
 	static String date = null; //売り上げ日の記入
 	static String month = null;//売り上げ月の記入
 	
 	static String startDate = null;
 	static String lastDate = null;
+	static String Month = null;
+	static String Customer = null;
 	
 	public String startDate() {
-		
 		if(!(startDate == null)) {
 			return startDate;
 		}
@@ -27,6 +27,7 @@ public class Sales {
 		startDate = sc.nextLine();
 		return startDate;
 	}
+	
 	public String lastDate() {
 		if(!(lastDate == null)) {
 			return lastDate;
@@ -36,41 +37,49 @@ public class Sales {
 		lastDate = sc.nextLine();
 		return lastDate;
 	}
-	public static String Month() {
+	
+	public String Month() {
+		if(!(Month == null)) {
+			return Month;
+		}
 		Scanner sc = new Scanner(System.in); //月
 		System.out.print("月： ");
-		String Month = sc.nextLine();
+		Month = sc.nextLine();
 		return Month;
 	}
-	public static String Customer() {
+	
+	public String Customer() {
+		if(!(Customer == null)) {
+			return Customer;
+		}
 		Scanner sc = new Scanner(System.in); //顧客
 		System.out.print("\n顧客名：  ");
-		String Customer = sc.nextLine();
+		Customer = sc.nextLine();
 		return Customer;
 	}
 	
 	public static void menuSales(String date, int menu) {
 		
 		while(true) {
-			Scanner sc = new Scanner(System.in); //項目を選択 
+			Scanner sc = new Scanner(System.in); //項目を選択
 			System.out.println("【注文管理】");
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
 			System.out.println("どの項目を検索しますか？\n "
 					+ "操作したい番号をお選びください。\n");
-			System.out.println("0.ホーム画面\n" + "1.月次\n" + "2.日次\n" + 
+			System.out.println("0.ホーム画面\n" + "1.月次\n" + "2.日次\n" +
 			"3.顧客名と月次\n" + "4.顧客名と日次\n" + "5.メニュー画面\n");
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー\n");
 			
 			System.out.print("番号を入力 ： ");
 			menu = sc.nextInt();
-
+ 
 			if ( menu == 1 ||menu == 2 || menu == 3 || menu == 4 ) {
 				break; //ループを抜けて詳細画面へ
 				} else if (menu == 0) {
-//				Main.main(null) ;//ホーム画面へ
-					} else if (menu == 5) {
-//						Menu_employee employee = new Menu_employee(); 
-//						employee.menu_employee(); //メニュー画面へ
+				  Main.main(null) ;//ホーム画面へ
+				  } else if (menu == 5) {
+					  Menu_employee employee = new Menu_employee();
+						employee.menu_employee(); //メニュー画面へ
 						} else {
 							System.out.println("\n【エラー：項目以外の内容の入力】");
 							System.out.println("0〜5の番号を入力してください。\n");
@@ -79,81 +88,77 @@ public class Sales {
 			}
 		
 		//詳細画面へ
-		if (menu == 1) { 
+		Sales sales = new Sales();
+		if (menu == 1) {
 			System.out.println("\n【月次】");
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
-			System.out.println("期間を指定して売上合計を計算します。\n" 
+			System.out.println("期間を指定して売上合計を計算します。\n"
 					+ "表示したいデータの年と月を入力してください。");
-			System.out.println("記入例：2025-06");
+			System.out.println("記入例⇒ 2025-06");
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
 		}  
 		   else if (menu == 2) {
 			    System.out.println("\n【日次】");
 				System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
-				System.out.println("期間を指定して売上合計を計算します。\n" 
+				System.out.println("期間を指定して売上合計を計算します。\n"
 						+ "表示したいデータの年、開始日、終了日を入力してください。\n");
-				System.out.println("記入例：2025-06-01, 2025-06-30");
+				System.out.println("記入例⇒ 開始日：2025-06-01 終了日：2025-06-30");
 				System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
 		}
 		   else if (menu == 3) {
 			    System.out.println("\n【顧客名と月次】");
 				System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
-				System.out.println("顧客名と期間を指定して売上合計を計算します。\n" 
+				System.out.println("顧客名と期間を指定して売上合計を計算します。\n"
 						+ "表示したいデータの顧客名と月を入力してください。\n");
-				System.out.println("記入例：2025-06");
-				Month();
-				Customer();
+				System.out.println("記入例⇒ 2025-06\n");
 				System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
 		}
 		   else if (menu == 4) {
 			    System.out.println("\n【顧客名と日次】");
 				System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
-				System.out.println("顧客名と期間を指定して売上合計を計算します。\n" 
+				System.out.println("顧客名と期間を指定して売上合計を計算します。\n"
 						+ "表示したいデータの顧客名、年、開始日、終了日を選択入力してください。\n");
-				System.out.println("記入例：2025-06-01, 2025-06-30");
-				Customer();
+				System.out.println("記入例⇒ 開始日：2025-06-01 終了日：2025-06-30");
 				System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
 		}
 		
 		//結果
-		if (menu == 1) { 
-            String month = Month(); // メソッドを呼び出して戻り値を取得
+		if (menu == 1) {
+            String month = sales.Month(); // メソッドを呼び出して戻り値を取得
 			System.out.println("\n【月次：合計】");
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
 			System.out.println( month );
-			System.out.println("\n売上合計" + Select.selectReceipt(9,null)[1]  + "円" ); 
+			System.out.println("\n売上合計" + Select.selectReceipt(9,null)[1]  + "円" );
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー\n");
 		}  
 		else if(menu == 2) {
-			Sales sales = new Sales();
 			String start = sales.startDate();
 			String last = sales.lastDate();
 			System.out.println("\n【日次：合計】");
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
 			System.out.println( start + "～" + last);
-			System.out.println("\n売上合計" + Select.selectReceipt(7,null)[1]  + "円" ); 
+			System.out.println("\n売上合計" + Select.selectReceipt(7,null)[1]  + "円" );
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー\n");
 		}
-		else if (menu == 3) { 
-            String month = Month(); 
-            String customer = Customer();
+		else if (menu == 3) {
+            String month = sales.Month();
+            String customer = sales.Customer();
 			System.out.println("\n【月次：合計】");
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
 			System.out.println( "顧客名 ： " + customer + "\n");
 			System.out.println( month );
-			System.out.println("\n売上合計" +Select.selectReceipt(10,customer)[1]  + "円" ); 
+			System.out.println("\n売上合計" +Select.selectReceipt(10,customer)[1]  + "円" );
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー\n");
 		}  
 		else if(menu == 4) {
-			Sales sales = new Sales();
 			String start = sales.startDate();
 			String last = sales.lastDate();
-			String customer = Customer();
+			String customer = sales.Customer();
 			System.out.println("\n【日次：合計】");
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー");
 			System.out.println( "顧客名 ： " + customer + "\n" );
 			System.out.println( start + "～" + last);
-			System.out.println("\n売上合計" + Select.selectReceipt(8,customer)[1]  + "円" ); 
+			System.out.println("\n売上合計" + Select.selectReceipt(8,customer)[1]  + "円" );
 			System.out.println("ーーーーーーーーーーーーーーーーーーーーーー\n");
 		}
 		
@@ -182,3 +187,4 @@ public class Sales {
 		}
 	}
 }
+ 
